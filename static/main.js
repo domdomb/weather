7bbc5bc9e52981e59e771386daabbe0cfedb585e
dominic lavezzoli <domiihl@gmail.com>
2019-11-18 21:30:35 -0800

    added the very small code base

---
+++ static/main.js
@@ -0,0 +1,39 @@
+window.addEventListener('load',()=> {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/d264c42ff0e70a254c83ffab177e38d2/${lat},${long}`;
+            
+                fetch(api)
+                    .then(response =>{
+                    return response.json();
+                })
+                .then(data => {
+                    const { temperature, summary, icon } = data.currently;
+                    //Set DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                        //setting icon
+                        setIcons(icon,document.querySelector('.icon'));
+                });
+        });
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color:"white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
